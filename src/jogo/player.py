784fa329a0741ea0026baea29d898c9cc2cdfc52
Ott-on <ottonvinicius1004@gmail.com>
2024-05-18 18:31:23 -0300

    feat: class player

--- src/jogo/player.py
+++ src/jogo/player.py
@@ -1,1 +1,9 @@
 from .base import Mob
+
+class Player(Mob):
+   def __init__(self, vida, dano):
+        Mob.__init__(self, 'recursos/imagens/sprite1.png', (1, 1), (70, 60), (0, 0), vida, dano, escala=(0, 0))
+
+class Controle:
+    def __init__(self):
+                               
